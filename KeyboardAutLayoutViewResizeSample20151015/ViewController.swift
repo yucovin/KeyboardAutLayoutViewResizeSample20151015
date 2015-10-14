0c5a217860b514e282906005885faeb9e9b314eb
vincent nino <yucovin@gmail.com>
2015-10-15 02:22:03 +0900

    Initial Commit

---
+++ KeyboardAutLayoutViewResizeSample20151015/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyboardAutLayoutViewResizeSample20151015
+//
+//  Created by vincent nino on 2015/10/15.
+//  Copyright © 2015年 yucovin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KeyboardAutLayoutViewResizeSample20151015Tests/KeyboardAutLayoutViewResizeSample20151015Tests.swift
@@ -0,0 +1,36 @@
+//
+//  KeyboardAutLayoutViewResizeSample20151015Tests.swift
+//  KeyboardAutLayoutViewResizeSample20151015Tests
+//
+//  Created by vincent nino on 2015/10/15.
+//  Copyright © 2015年 yucovin. All rights reserved.
+//
+
+import XCTest
+@testable import KeyboardAutLayoutViewResizeSample20151015
+
+class KeyboardAutLayoutViewResizeSample20151015Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
