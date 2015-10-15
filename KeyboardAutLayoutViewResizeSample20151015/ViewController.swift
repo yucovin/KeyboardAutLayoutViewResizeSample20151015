52605cc9f89cd82058350be0f0b5b625a36d3170
vincent nino <yucovin@gmail.com>
2015-10-15 10:51:21 +0900

    textViewにkeyboardDismissModeなんてのがあった。 （iOS7~：UIScrollViewにkeyboardDismissModeプロパティ）

--- KeyboardAutLayoutViewResizeSample20151015/ViewController.swift
+++ KeyboardAutLayoutViewResizeSample20151015/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController, UITextViewDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //スワイプでKeyboardをしまえる
+        textView.keyboardDismissMode = UIScrollViewKeyboardDismissMode.OnDrag
         
         //キーボードの出し入れをNotificationCenterからうけとる。UIKeyboardDidShowNotification、UIKeyboardDidHideNotificationもある。
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
@@ -66,14 +68,14 @@ class ViewController: UIViewController, UITextViewDelegate {
     }
     
     
-    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
-        if text == "\n" {
-            textView.resignFirstResponder()
-            return false
-        }
-        return true
-    }
-    
+//    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+//        if text == "\n" {
+//            textView.resignFirstResponder()
+//            return false
+//        }
+//        return true
+//    }
+//    
 
 }
 
